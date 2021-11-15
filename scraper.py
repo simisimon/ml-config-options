d914f1189d82f8a482de05b786aff09e67d75286
mj60dony <mj60dony@studserv.uni-leipzig.de>
2021-11-15 20:06:13 +0100

    -

--- scraper.py
+++ scraper.py
@@ -1,38 +1,49 @@
-# Find example
-
-# Import the libraries BeautifulSoup
-# and os
-from bs4 import BeautifulSoup as bs
-import os
-
-# Open the HTML in which you want to
-# make changes
-#html = 'https://pytorch.org/docs/stable/torch.html'
-
-# Parse HTML file in Beautiful Soup
-#soup = bs(html, 'html.parser')
-
-# Obtain the text from the widget after
-# finding it
-#find_example = soup.find("p", {"id": "torch"}).get_text()
-
-# Printing the text obtained received
-# in previous step
-#print(find_example)
-
+import pprint
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 
-url = "https://pytorch.org/docs/stable/torch.html"
-html = urlopen(url)
-soup = BeautifulSoup(html, "html.parser")
+def extract_doc(url_suffix):
+    # Open the HTML
+    torch_dict = {}
+    for u in url_suffix:
+        link = url + u
+        try:
+            html = urlopen(link)
+        except:
+            continue
 
-tables = soup.find_all('table')
-torch_dict = {}
-for table in tables:
-    for row in table.find_all('tr'):
-        row_cut = row.get_text()[:-1]
-        k = row_cut.split("\n")
-        torch_dict[k[0]] = k[1]
+        # Parse HTML file in Beautiful Soup
+        soup = BeautifulSoup(html, "html.parser")
+        # Obtain the text from the widget after finding it
+        tables = soup.find_all("table")
+        u = u.split(".html")[0]
+        torch_dict[u] = {}
 
-print(torch_dict)
+        for table in tables:
+            for row in table.find_all("tr"):
+                row_cut = row.get_text()[:-1]
+                k = row_cut.split("\n")
+                torch_dict[u][k[0]] = k[1]
+
+    return torch_dict
+
+def get_torch_url(url):
+    html = urlopen(url)
+    soup = BeautifulSoup(html, "html.parser")
+
+    modules = soup.find_all('div', {'class': 'toctree-wrapper compound'})[2]
+    modules = modules.find_all('a')
+    url_suffix = []
+    for module in modules:
+        url_suffix.append(module['href'])
+
+    return url_suffix
+
+url = "https://pytorch.org/docs/stable/"
+url_suffix = get_torch_url(url)
+torch_dict = extract_doc(url_suffix)
+pprint.pprint(torch_dict)
+
+
+
+
