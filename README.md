4e2d97e2eb5b00b6814a4559209b70d2b5f428f7
mj60dony <mj60dony@studserv.uni-leipzig.de>
2021-11-29 15:22:13 +0100

    -

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # coop
-1) extract.py -> extract information from ML-lib docs
-2) analyze_import.py -> extract keywords from import lines of the source code that use those ML-libs
+1) extract.py
+   - extract information from ML-lib docs
+2) analyze_import.py 
+   - extract keywords from import lines of the source code that use those ML-libs
+   - loop through source code and extract lines containing those keywords
+
+current status: 
+    - identified lines of code that use ML-libs
+
+next steps:
+    - distinguish potential config options from irrelevant code in a most generic way
--- analyze_import.py
+++ analyze_import.py
@@ -33,18 +33,36 @@ def get_search_words(ml_lib_import_lines, ml_lib):
     return search_words
 
 def modify_import_lines(ml_lib_import_lines, initial_word):
-    lines = [line for line in ml_lib_import_lines if line.startswith(initial_word)] #all lines starting with import/from and containing library
-    lines = [s.replace(" ", "") for s in lines]  # removing blank spaces
-    lines = [s.replace(initial_word, "") for s in lines]  # removing import/from keyword
-    lines = [s.split(",") for s in lines]  # seperating the libraries into nested list
-    lines = [s for sublist in lines for s in sublist]  # flatten the nested list
+    lines_list = [line for line in ml_lib_import_lines if line.startswith(initial_word)] #all lines starting with import/from and containing library
+    lines_list = [s.replace(" ", "") for s in lines_list]  # removing blank spaces
+    lines_list = [s.replace(initial_word, "") for s in lines_list]  # removing import/from keyword
+    lines_list = [s.split(",") for s in lines_list]  # seperating the libraries into nested list
+    lines_list = [s for sublist in lines_list for s in sublist]  # flatten the nested list
+
+    return lines_list
+
+def reduce_code(code):
+    code = [line for line in code.split('\n') if not (line.startswith("from") or line.startswith("import") or line.startswith("#") or line == "")]
+    return code
+
+def get_relevant_lines(code, search_words):
+    lines = []
+    for line in code:
+        for search_word in search_words:
+            if search_word in line:
+                lines.append(line)
 
     return lines
+
 
 source_file = "test_projects/sklearn_lin_reg.py"
 ml_lib = "sklearn"
 code = get_source_code(source_file)
 import_lines = get_import_lines(code, ml_lib)
-pprint(import_lines)
 search_words = get_search_words(import_lines, ml_lib)
 pprint(search_words)
+code = reduce_code(code)
+lines = get_relevant_lines(code, search_words)
+pprint(lines)
+
+
