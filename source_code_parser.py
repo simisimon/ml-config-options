a362ee1212941ee52f48bf69ddaa8e0eb0cb5810
mj-support <marco-hunter@web.de>
2021-12-16 18:49:37 +0100

    - Refactore code to focus more on get_values() in source_code_parser

--- source_code_parser.py
+++ source_code_parser.py
@@ -4,7 +4,7 @@ from pprint import pprint
 import re
 
 # https://stackoverflow.com/questions/33506902/python-extracting-editing-all-constants-involved-in-a-function-via-ast
-project = "test_projects/sklearn_lin_reg.py"
+project = "test_projects/sklearn_birch.py"
 final_vars = [] ##dict
 
 
@@ -26,16 +26,16 @@ def extract(objects):
 
 def extract_assigns(obj):
     if type(obj.targets[0]) != ast.Tuple: #Case 1: Normalfall
-        values = obj_type[type(obj.value)](obj.value)
-        assign_vars = extraxt_assign_vars(obj.targets[0])
+        values = get_values(obj.value)
+        assign_vars = get_values(obj.targets[0])
         final_vars.append(assign_vars + " = " + str(values))
 
     elif type(obj.value) != ast.Tuple and type(obj.value) != ast.List: #Case 2: Zusammenfassung
-        values = obj_type[type(obj.value)](obj.value)
+        values = get_values(obj.value)
         if type(obj.value) != ast.Constant: #Tupel = Konstante syntaktisch falsch und wird geskippt
             assign_vars = ""
             for assign in obj.targets[0].elts:
-                assign_vars += str((extraxt_assign_vars(assign))) + ", "
+                assign_vars += str(get_values(assign)) + ", "
             final_vars.append(assign_vars[:-2] + " = " + str(values))
         else:
             print("Syntaktisch nicht zulässig")
@@ -43,24 +43,18 @@ def extract_assigns(obj):
     elif len(obj.targets[0].elts) == len(obj.value.elts): #Case 3: Mehrere Assignments
         targets = obj.targets[0].elts
         for assign in targets:
-            assign_var = extraxt_assign_vars(assign)
+            assign_var = get_values(assign)
             value = obj.value.elts[targets.index(assign)]
-            value = obj_type[type(value)](value)
+            value = get_values(value)
             final_vars.append(assign_var + " = " + str(value))
 
     else:
         print("NEUER FALL!!!!!! bzw. Falsches Assignment") #kein Assignment
 
 
-def extraxt_assign_vars(assign_obj):
-    ast_type = type(assign_obj)
-    values = obj_type[ast_type](assign_obj)
-    #if type(assign_obj) == ast.Attribute:
-     #   assign_id = extract_attr(assign_obj)
-    #elif type(assign_obj) == ast.Tuple:
-     #   assign_id = extract_tuple(assign_obj)
-    #else:
-     #   assign_id = assign_obj.id
+def get_values(obj_value):
+    ast_type = type(obj_value)
+    values = obj_type[ast_type](obj_value)
     return values
 
 
@@ -68,8 +62,7 @@ def extract_expr(obj):
     if type(obj.value) == ast.Constant:
         print("docstring?")
     else:
-        ast_type = type(obj.value)
-        values = obj_type[ast_type](obj.value)
+        values = get_values(obj.value)
         final_vars.append(values)
 
 
@@ -87,11 +80,9 @@ def extract_func(obj):
 
 
 def extract_tuple(obj_val):
-
     values = "("
     for o in obj_val.elts:
-        ast_type = type(o)
-        values += str(obj_type[ast_type](o)) + ", "
+        values += str(get_values(o)) + ", "
     values = values[:-2] + ")"
     return values
 
@@ -99,43 +90,36 @@ def extract_tuple(obj_val):
 def extract_list(obj_val):
     values = []
     for o in obj_val.elts:
-        ast_type = type(o)
-        values.append(obj_type[ast_type](o))
+        values.append(get_values(o))
 
         if type(o) == ast.List:
             list_values = []
             for v in o.elts:
-                ast_type = type(v)
-                list_values.append(obj_type[ast_type](v))
+                list_values.append(get_values(v))
             values.append(list_values)
     return values
 
 
 def extract_call(obj_val):
-    ast_type = type(obj_val.func)
-    values = obj_type[ast_type](obj_val.func)
+    values = get_values(obj_val.func)
 
     values += "("
     for param in obj_val.args:
         if values[-1] != "(":
             values += ", "
-        ast_type = type(param)
-        param_value = obj_type[ast_type](param)
+        param_value = get_values(param)
         values += str(param_value)
     for param in obj_val.keywords:
         if values[-1] != "(":
             values += ", "
-        ast_type = type(param.value)
-        param_value = obj_type[ast_type](param.value)
+        param_value = get_values(param.value)
         values += param.arg + "=" + str(param_value)
     values += ")"
     return values
 
 
 def extract_bin_op(obj_val):
-    values = "BinOp???"  # ast.dump(ast.parse('x ** n + y ** n + k', mode='eval'), indent=4))
-    ast_type = type(obj_val.left)
-    values = obj_type[ast_type](obj_val.left)
+    values = get_values(obj_val.left)
     if type(obj_val.op) == ast.Add:
         values += " + "
     elif type(obj_val.op) == ast.Sub:
@@ -163,9 +147,7 @@ def extract_bin_op(obj_val):
     elif type(obj_val.op) == ast.MatMult:
         values += " @ "
 
-    ast_type = type(obj_val.right)
-    values += obj_type[ast_type](obj_val.right)
-
+    values += str(get_values(obj_val.right))
     return values
 
 
@@ -191,8 +173,7 @@ def extract_name(obj_val):
 
 
 def extract_attr(obj_val):
-    ast_type = type(obj_val.value)
-    values = obj_type[ast_type](obj_val.value)
+    values = get_values(obj_val.value)
     values += "." + obj_val.attr
     return values
 
@@ -201,23 +182,20 @@ def extract_dict(obj_val):
     values = {}
     count = 0
     for key in obj_val.keys:
-        ast_type = type(key)
-        k = obj_type[ast_type](key)
+        k = get_values(key)
 
         v = obj_val.values[count]
-        ast_type = type(v)
-        v = obj_type[ast_type](v)
+        v = get_values(v)
 
         values[k] = v
         count += 1
 
     return values
 
+
 def extract_subscript(obj_val):
-    ast_type = type(obj_val.value)
-    values = obj_type[ast_type](obj_val.value) + "["
-    ast_type = type(obj_val.slice)
-    values += obj_type[ast_type](obj_val.slice) + "]"
+    values = get_values(obj_val.value) + "["
+    values += get_values(obj_val.slice) + "]"
     return values
 
 
@@ -227,41 +205,34 @@ def extract_slice(obj_val):
     upper = obj_val.upper
     step = obj_val.step
     if lower != None:
-        ast_type = type(lower)
-        values += str(obj_type[ast_type](lower))
+        values += str(get_values(lower))
     values += ":"
     if upper != None:
-        ast_type = type(upper)
-        values += str(obj_type[ast_type](upper))
+        values += str(get_values(upper))
     if step != None:
-        ast_type = type(step)
-        values += ":" + str(obj_type[ast_type](step))
+        values += ":" + str(get_values(step))
     return values
 
 
 def extract_ext_slice(obj_val):
     values = ""
     for d in obj_val.dims:
-        ast_type = type(d)
-        values += str(obj_type[ast_type](d)) + ", "
+        values += str(get_values(d)) + ", "
     return values[:-2]
 
 
 def extract_index(obj_val):
-    ast_type = type(obj_val.value)
-    values = obj_type[ast_type](obj_val.value)
+    values = get_values(obj_val.value)
     return values
 
 
 def extract_with(obj_val):
     values = ""
     for item in obj_val.items:
-        ast_type = type(item)
-        final_vars.append(obj_type[ast_type](item))
+        final_vars.append(get_values(item))
 
     for v in obj_val.body:
-        ast_type = type(v)
-        val_temp = obj_type[ast_type](v)
+        val_temp = get_values(v)
         if val_temp != None:
             values += val_temp
     return values
@@ -269,12 +240,10 @@ def extract_with(obj_val):
 
 def extract_withitem(obj_val):
     values = "with "
-    ast_type = type(obj_val.context_expr)
-    values += obj_type[ast_type](obj_val.context_expr)
+    values += get_values(obj_val.context_expr)
 
     if obj_val.optional_vars != None:
-        ast_type = type(obj_val.optional_vars)
-        values += " as " + obj_type[ast_type](obj_val.optional_vars)
+        values += " as " + get_values(obj_val.optional_vars)
     return values + ":"
 
 
@@ -298,7 +267,8 @@ obj_type = {ast.Expr: extract_expr,
             ast.withitem: extract_withitem,
             ast.Import: dummy,
             ast.ImportFrom: dummy,
-            ast.Assert: dummy}
+            ast.Assert: dummy
+            }
 
 objects = get_objects(project)
 result = extract(objects)
