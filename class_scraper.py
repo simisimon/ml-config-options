afec6e355ef38f1323a8cdfd12d790e47cebc949
mj-support <marco-hunter@web.de>
2022-02-18 21:10:52 +0100

    - add inheritance structure

--- class_scraper.py
+++ class_scraper.py
@@ -3,124 +3,144 @@ from bs4 import BeautifulSoup
 import json
 
 
-def get_sklearn_class_urls():
-    link = "https://scikit-learn.org/stable/modules/classes.html#"
-    html = urlopen(link)
-    soup = BeautifulSoup(html, "html.parser")
-    table_rows = soup.body.findAll("tr")
-
-    class_urls = []
-    for t in table_rows:
-        obj = t.find("a").text
-        index = [i for i, c in enumerate(obj) if c.isupper()]
-        for i in index:
-            if obj[i-1] == ".":
-                url = t.find("a").attrs["href"]
-                class_urls.append(url)
-                break
-
-    return class_urls
-
-
-def get_sklearn_classes(urls):
-    sklearn_classes = {}
-    for u in urls:
-        url = "https://scikit-learn.org/stable/modules/" + u
-        html = urlopen(url)
-        soup = BeautifulSoup(html, "html.parser")
-
-        data_table = soup.find("dt", {"class": "sig sig-object py"})
-        elements = data_table.findAll("em", {"class": "sig-param"})
-        class_name = data_table.find("span", {"class": "sig-name descname"}).text
-        parameter = {}
+class ClassScraper:
+    def scrape_parameters(self, elements):
+        parameters = {}
         for e in elements:
             split = e.text.split("=", 1)
-            p = split[0]
+            param = split[0]
             if len(split) > 1:
                 default_value = split[1]
-                parameter[p] = default_value
+                parameters[param] = default_value
             else:
-                parameter[p] = None
+                parameters[param] = None
+        return parameters
 
-        sklearn_classes[class_name] = parameter
-
-    return sklearn_classes
-
-
-def get_torch_module_urls():
-    link = "https://pytorch.org/docs/stable/index.html"
-    html = urlopen(link)
-    soup = BeautifulSoup(html, "html.parser")
-
-    caption = soup.find("p", {"class": "caption"}, text="Python API")
-    ul = caption.find_next("ul")
-    li = ul.findAll("li", {"class": "toctree-l1"})
-    module_url = []
-    for element in li:
-        url = element.find("a").attrs["href"]
-        module_url.append(url)
-
-    return module_url
+    def create_json(self, library, classes):
+        with open("{0}.txt".format(library), 'w') as outfile:
+            json.dump(classes, outfile, indent=4)
 
 
-def get_torch_class_urls(urls):
-    torch_cls = {}
-    cls_urls = []
-    for u in urls:
-        try:
-            url = "https://pytorch.org/docs/stable/" + u
-            html = urlopen(url)
+class SklearnScraper(ClassScraper):
+    def __init__(self):
+        self.class_urls = []
+        self.classes = {}
+
+    def get_classes(self):
+        self.scrape_class_urls()
+        self.scrape_classes()
+        self.create_json("sklearn", self.classes)
+
+    def scrape_class_urls(self):
+        link = "https://scikit-learn.org/stable/modules/classes.html#"
+        html = urlopen(link)
+        soup = BeautifulSoup(html, "html.parser")
+
+        table_rows = soup.body.findAll("tr")
+        for row in table_rows:
+            text = row.find("a").text
+            text = text[text.rfind(".") + 1:]
+            if text[0].isupper():
+                url = row.find("a").attrs["href"]
+                self.class_urls.append(url)
+
+    def scrape_classes(self):
+        for url in self.class_urls:
+            link = "https://scikit-learn.org/stable/modules/" + url
+            html = urlopen(link)
             soup = BeautifulSoup(html, "html.parser")
 
-            table_rows = soup.body.findAll("tr")
+            data_table = soup.find("dt", {"class": "sig sig-object py"})
+            elements = data_table.findAll("em", {"class": "sig-param"})
+            class_ = data_table.find("span", {"class": "sig-name descname"}).text
 
-            for row in table_rows:
-                a = row.find("a")
-                if a != None and "title" in a.attrs:
-                    title = a.attrs["title"]
-                    title = title[title.rfind("."):]
-                    if title[1].isupper():
-                        url = row.find("a").attrs["href"]
-                        cls_urls.append(url)
+            parameters = self.scrape_parameters(elements)
+            self.classes[class_] = parameters
 
+
+class TorchScraper(ClassScraper):
+    def __init__(self):
+        self.module_urls = []
+        self.class_urls = []
+        self.desc_elements = []
+        self.classes = {}
+
+    def get_classes(self):
+        self.scrape_module_urls()
+        self.scrape_class_urls()
+        self.scrape_desc_elements()
+        self.scrape_classes()
+        self.create_json("pytorch", self.classes)
+
+    def scrape_module_urls(self):
+        link = "https://pytorch.org/docs/stable/index.html"
+        html = urlopen(link)
+        soup = BeautifulSoup(html, "html.parser")
+
+        caption = soup.find("p", {"class": "caption"}, text="Python API")
+        ul = caption.find_next("ul")
+        li = ul.findAll("li", {"class": "toctree-l1"})
+
+        for element in li:
+            url = element.find("a").attrs["href"]
+            self.module_urls.append(url)
+
+    def scrape_class_urls(self):
+        for url in self.module_urls:
+            try:
+                link = "https://pytorch.org/docs/stable/" + url
+                html = urlopen(link)
+                soup = BeautifulSoup(html, "html.parser")
+
+                if url == "distributed.elastic.html":
+                    caption = soup.find("p", {"class": "caption"}, text="API")
+                    ul = caption.find_next("ul")
+                    li = ul.findAll("li", {"class": "toctree-l1"})
+                    for element in li:
+                        url = element.find("a").attrs["href"]
+                        self.class_urls.append(url)
+
+                else:
+                    table_rows = soup.body.findAll("tr")
+                    for row in table_rows:
+                        a = row.find("a")
+                        if a is not None and "title" in a.attrs:
+                            title = a.attrs["title"]
+                            title = title[title.rfind(".") + 1:]
+                            if title[0].isupper():
+                                url = row.find("a").attrs["href"]
+                                self.class_urls.append(url)
+
+                    dl = soup.findAll("dl", {"class": "py class"})
+                    self.desc_elements.extend(dl)
+            except:
+                continue
+
+    def scrape_desc_elements(self):
+        for url in self.class_urls:
+            link = "https://pytorch.org/docs/stable/" + url
+            html = urlopen(link)
+            soup = BeautifulSoup(html, "html.parser")
             dl = soup.findAll("dl", {"class": "py class"})
             if len(dl) > 0:
-                for d in dl:
-                    dt = d.find("dt")
-                    if "id" in dt.attrs:
-                        cls = dt.attrs["id"]
-                        cls = cls[cls.rfind(".") + 1:]
-                        elements = d.findAll("em", {"class": "sig-param"})
-                        param = {}
-                        for ele in elements:
-                            split = ele.text.split("=", 1)
-                            p = split[0]
-                            if len(split) > 1:
-                                default_value = split[1]
-                                param[p] = default_value
-                            else:
-                                param[p] = None
-                        torch_cls[cls] = param
-        except:
-            continue
-    return 0
+                for element in dl:
+                    self.desc_elements.append(element)
 
-def create_json(classes):
-    with open("sklearn.txt", 'w') as outfile:
-        json.dump(classes, outfile, indent=4)
+    def scrape_classes(self):
+        for desc_element in self.desc_elements:
+            dt = desc_element.find("dt")
+            if "id" in dt.attrs:
+                class_ = dt.attrs["id"]
+                class_ = class_[class_.rfind(".") + 1:]
+                elements = desc_element.findAll("em", {"class": "sig-param"})
+                parameters = self.scrape_parameters(elements)
+                self.classes[class_] = parameters
 
 
 def main():
-    #sklearn_class_urls = get_sklearn_class_urls()
-    #sklearn_classes = get_sklearn_classes(sklearn_class_urls)
-    #create_json(sklearn_classes)
-    torch_module_urls = get_torch_module_urls()
-    torch_class_urls = get_torch_class_urls(torch_module_urls)
-
+    SklearnScraper().get_classes()
+    TorchScraper().get_classes()
 
 
 if __name__ == "__main__":
     main()
-
-
-
